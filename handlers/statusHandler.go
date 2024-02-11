5a3010a986fa768546a1c39e55c9d0d55cc026a5
patrickjoh <patrick.johannessen@hotmail.com>
2024-02-11 10:56:00 +0100

    feat(status): Get requests now time out if APIs take a long time to answer

--- handlers/statusHandler.go
+++ handlers/statusHandler.go
@@ -14,60 +14,48 @@ func init() {
 	startTime = time.Now()
 }
 
+// Utility function to perform HTTP GET requests and handle common errors.
+func fetchAPIStatus(url string) string {
+
+	client := &http.Client{
+		Timeout: time.Second * 10, // Set time for requests to time out
+	}
+
+	resp, err := client.Get(url)
+	if err != nil {
+		return http.StatusText(http.StatusServiceUnavailable)
+	}
+	defer resp.Body.Close()
+	return resp.Status
+}
+
 // Handler function for the status endpoint
 func StatusHandler(w http.ResponseWriter, r *http.Request) {
-
 	switch r.Method {
 	case http.MethodGet:
 		handleGetStatus(w)
 	default:
-		http.Error(w, "REST method "+r.Method+" is not supported. Only "+http.MethodGet+" is supported.",
-			http.StatusNotImplemented)
+		http.Error(w, "REST method "+r.Method+" is not supported. Only "+http.MethodGet+" is supported.", http.StatusNotImplemented)
 		return
 	}
 }
 
 func handleGetStatus(w http.ResponseWriter) {
-
-	status := as1.Status{}
-
-	gutendexRes, err := http.Get(as1.GUTENDEX_STATUS)
-	if err != nil {
-		status.GutendexAPI = http.StatusText(http.StatusServiceUnavailable)
-	} else {
-		status.GutendexAPI = gutendexRes.Status
-		defer gutendexRes.Body.Close()
+	status := as1.Status{
+		GutendexAPI:  fetchAPIStatus(as1.GUTENDEX_STATUS),
+		L2cAPI:       fetchAPIStatus(as1.L2C_STATUS),
+		CountriesAPI: fetchAPIStatus(as1.COUNTRY_STATUS),
+		Version:      as1.STATUS_VERSION,
+		Uptime:       time.Since(startTime),
 	}
 
-	l2cRes, err := http.Get(as1.L2C_STATUS)
-	if err != nil {
-		status.L2cAPI = http.StatusText(http.StatusServiceUnavailable)
-	} else {
-		status.L2cAPI = l2cRes.Status
-		defer l2cRes.Body.Close()
-	}
-
-	countryRes, err := http.Get(as1.COUNTRY_STATUS)
-	if err != nil {
-		status.CountriesAPI = http.StatusText(http.StatusServiceUnavailable)
-	} else {
-		status.CountriesAPI = countryRes.Status
-		defer countryRes.Body.Close()
-	}
-
-	status.Version = as1.STATUS_VERSION
-	status.Uptime = time.Since(startTime)
-
-	// Encode struct to JSON
 	jsonBytes, err := json.Marshal(status)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Error in outputting JSON: %s", err), http.StatusInternalServerError)
 		return
 	}
 
-	// Write JSON to ResponseWriter
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(jsonBytes)
-
 }
